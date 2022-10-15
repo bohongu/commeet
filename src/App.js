51f0c80aa5114afff140e1e83336e0aea74d42ca
bohongu <jaehong2782@naver.com>
2022-10-15 15:22:01 +0900

    firebase setting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { fireStore } from './Firebase';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 
 const App = () => {
+  useEffect(() => {
+    console.log(fireStore);
+  });
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
