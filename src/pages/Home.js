be03868301c4f3a3a895ba3b5288827786844909
bohongu <jaehong2782@naver.com>
2022-10-18 21:34:23 +0900

    readme

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Home = () => {
-  return <div>home</div>;
+  const [title, setTitle] = useState('');
+  const [commeet, setCommeet] = useState('');
+  const onChange = (event) => {
+    const { name, value } = event.target;
+    switch (name) {
+      case 'title':
+        return setTitle(value);
+      case 'commeet':
+        return setCommeet(value);
+      default:
+        return;
+    }
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <label htmlFor="title">제목</label>
+      <input name="title" id="title" value={title} onChange={onChange} />
+      <label htmlFor="commeet">본문</label>
+      <input name="commeet" id="commeet" value={commeet} onChange={onChange} />
+      <label htmlFor="image">사진</label>
+      <input id="image" type="file" />
+      <button>업로드</button>
+    </form>
+  );
 };
 
 export default Home;
