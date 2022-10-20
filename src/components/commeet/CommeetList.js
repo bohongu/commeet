e114d2dea7af2e581ecd94dd9c9c445df82785cb
bohongu <jaehong2782@naver.com>
2022-10-20 19:59:39 +0900

    임시 모달 css 모듈 &&  게시글 수정하기 기능

--- src/components/commeet/CommeetList.js
+++ src/components/commeet/CommeetList.js
@@ -1,13 +1,64 @@
-import { deleteDoc, doc } from 'firebase/firestore';
+import { deleteDoc, doc, updateDoc } from 'firebase/firestore';
 import React from 'react';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { db } from '../../Firebase';
+import Modal from '../ui/Modal';
+
+const UpdateForm = ({ commeet, dbRef, showSetting, setShowUpdateForm }) => {
+  const [newTitle, setNewTitle] = useState('');
+  const [newCommeet, setNewCommeet] = useState('');
+  const onChange = (event) => {
+    const { name, value } = event.target;
+    switch (name) {
+      case 'newTitle':
+        return setNewTitle(value);
+      case 'newCommeet':
+        return setNewCommeet(value);
+      default:
+        return;
+    }
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await updateDoc(dbRef, {
+      title: newTitle,
+      commeet: newCommeet,
+    });
+    showSetting(false);
+    setShowUpdateForm(false);
+  };
+  return (
+    <Modal>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="newTitle">제목</label>
+        <input
+          name="newTitle"
+          id="newTitle"
+          value={newTitle}
+          onChange={onChange}
+          placeholder={commeet.title}
+        />
+        <label htmlFor="newCommeet">본문</label>
+        <input
+          name="newCommeet"
+          id="newCommeet"
+          value={newCommeet}
+          onChange={onChange}
+          placeholder={commeet.commeet}
+        />
+        <button>수정</button>
+      </form>
+    </Modal>
+  );
+};
 
 const CommeetList = ({ commeet }) => {
   const [setting, showSetting] = useState(false);
+  const [showUpdateForm, setShowUpdateForm] = useState(false);
   const userInfo = useSelector((state) => state.user.userInfo);
   const dbRef = doc(db, 'commeets', `${commeet.id}`);
+  const owner = commeet.authorId === userInfo.uid;
   const onSetting = () => showSetting(true);
   const onCancel = () => showSetting(false);
   const onDelete = async () => {
@@ -16,31 +67,42 @@ const CommeetList = ({ commeet }) => {
     if (ok) {
       await deleteDoc(dbRef);
     }
+    showSetting(false);
   };
-  const onUpdate = () => {};
+  const onUpdate = () => {
+    setShowUpdateForm(true);
+  };
   return (
-    <section
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      }}
-    >
-      <h3>{commeet.title}</h3>
-      <h3>{commeet.commeet}</h3>
-      <h3>{commeet.author}</h3>
-      <h3>{commeet.createdAt}</h3>
-      <h3>{commeet.authorId}</h3>
-      <h3>{userInfo.uid}</h3>
-      <div onClick={onSetting}>환경설정</div>
-      {setting && (
-        <>
-          <div onClick={onDelete}>삭제</div>
-          <div>수정</div>
-          <div onClick={onCancel}>취소</div>
-        </>
+    <>
+      {showUpdateForm && (
+        <UpdateForm
+          commeet={commeet}
+          dbRef={dbRef}
+          showSetting={showSetting}
+          setShowUpdateForm={setShowUpdateForm}
+        />
       )}
-    </section>
+      <section
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <h3>{commeet.title}</h3>
+        <h3>{commeet.commeet}</h3>
+        <h3>{commeet.author}</h3>
+        <h3>{commeet.createdAt}</h3>
+        {owner && <div onClick={onSetting}>환경설정</div>}
+        {setting && (
+          <>
+            <div onClick={onDelete}>삭제</div>
+            <div onClick={onUpdate}>수정</div>
+            <div onClick={onCancel}>취소</div>
+          </>
+        )}
+      </section>
+    </>
   );
 };
 
--- src/components/ui/Modal.js
+++ src/components/ui/Modal.js
@@ -1,34 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import styled from 'styled-components';
+import style from './Modal.module.css';
 
-const Backdrop = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100vh;
-  z-index: 20;
-  background-color: rgba(0, 0, 0, 0.75);
-`;
-
-const ModalBackdrop = (props) => {
-  return <Backdrop />;
+const Backdrop = (props) => {
+  return <div className={style.backdrop} onClick={props.onClose} />;
 };
 
 const ModalOverlay = (props) => {
-  return <div>{props.children}</div>;
+  return (
+    <div className={style.modal}>
+      <div className={style.content}>{props.children}</div>
+    </div>
+  );
 };
 
-const portal = document.getElementById('modal');
+const portalElement = document.getElementById('modal');
 
 const Modal = (props) => {
   return (
     <>
-      {ReactDOM.createPortal(<Backdrop />, portal)}
+      {ReactDOM.createPortal(
+        <Backdrop onClose={props.onClose} />,
+        portalElement,
+      )}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
-        portal,
+        portalElement,
       )}
     </>
   );
