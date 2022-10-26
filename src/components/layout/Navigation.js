6a4c0c394f5246271d9f4becd1175b921e677d32
bohongu <jaehong2782@naver.com>
2022-10-26 15:01:17 +0900

    프로필 사진 & 이름 변경

--- src/components/layout/Navigation.js
+++ src/components/layout/Navigation.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
+import { TbUserCircle } from 'react-icons/tb';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import UserModal from './UserModal';
 
 const Navigation = () => {
+  const userInfo = useSelector((state) => state.user.userInfo);
   const [showProfile, setShowProfile] = useState(false);
   const onShowProfile = () => {
     setShowProfile(true);
@@ -21,7 +24,18 @@ const Navigation = () => {
           <li>
             <Link to="/commeeting">글쓰기</Link>
           </li>
-          <li onClick={onShowProfile}>프로필</li>
+          <li onClick={onShowProfile}>
+            {userInfo.photoURL ? (
+              <img
+                src={userInfo.photoURL}
+                height="30px"
+                width="30px"
+                alt="profile"
+              />
+            ) : (
+              <TbUserCircle style={{ height: '30px', width: '30px' }} />
+            )}
+          </li>
         </ul>
       </nav>
       {showProfile && <UserModal onCloseProfile={onCloseProfile} />}
--- src/components/routes/Profile.js
+++ src/components/routes/Profile.js
@@ -1,17 +1,117 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { TbUserCircle } from 'react-icons/tb';
+import { AiOutlineCamera } from 'react-icons/ai';
+import { updateProfile } from 'firebase/auth';
+import { auth, storage } from '../../Firebase';
+import { useNavigate } from 'react-router-dom';
+import { getDownloadURL, ref, uploadString } from 'firebase/storage';
+import { v4 } from 'uuid';
 
 const Profile = () => {
-  const { uid } = useParams();
+  const navigate = useNavigate();
+  const [file, setFile] = useState('');
+  const [newName, setNewName] = useState('');
+  const [updateMode, setUpdateMode] = useState(false);
   const userInfo = useSelector((state) => state.user.userInfo);
-  console.log(userInfo);
+
+  const onUpdateMode = () => {
+    setUpdateMode(true);
+  };
+  const onCancel = () => {
+    setUpdateMode(false);
+  };
+  const onChange = (event) => {
+    setNewName(event.target.value);
+  };
+  const onUpdate = async () => {
+    updateProfile(auth.currentUser, {
+      displayName: newName,
+    });
+    navigate('/', { replace: true });
+  };
+
+  const showPhotoNav = () => {};
+
+  const onImageChange = (event) => {
+    const { files } = event.target;
+    const image = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (event) => {
+      setFile(event.currentTarget.result);
+    };
+    reader.readAsDataURL(image);
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    let photoUrl = '';
+    if (file !== '') {
+      const storageRef = ref(storage, `profile/${v4()}`);
+      await uploadString(storageRef, file, 'data_url');
+      photoUrl = await getDownloadURL(ref(storage, storageRef));
+    }
+    await updateProfile(auth.currentUser, {
+      photoURL: photoUrl,
+    });
+    setFile('');
+    navigate('/', { replace: true });
+  };
+
+  const onFileClear = () => {
+    setFile('');
+  };
+
+  const onDeleteImage = async () => {
+    await updateProfile(auth.currentUser, {
+      photoURL: null,
+    });
+    navigate('/', { replace: true });
+  };
   return (
-    <div>
-      <h1>하이하이</h1>
-      <h2>{userInfo.displayName}</h2>
-      <img src={userInfo.photoURL} width="50px" height="50px" />
-    </div>
+    <>
+      <h1>개인정보 수정</h1>
+      {userInfo.photoURL ? (
+        <img src={userInfo.photoURL} height="80px" width="80px" alt="profile" />
+      ) : (
+        <TbUserCircle style={{ height: '80px', width: '80px' }} />
+      )}
+      <AiOutlineCamera
+        style={{ height: '30px', width: '30px' }}
+        onClick={showPhotoNav}
+      />
+
+      <div>
+        <h2>기본정보</h2>
+        <div>
+          <span>이름</span>
+          {updateMode ? (
+            <input value={newName} onChange={onChange} />
+          ) : (
+            <span>{userInfo.displayName}</span>
+          )}
+          {updateMode ? (
+            <>
+              <button onClick={onUpdate}>확인</button>{' '}
+              <button onClick={onCancel}>취소</button>
+            </>
+          ) : (
+            <button onClick={onUpdateMode}>수정</button>
+          )}
+        </div>
+      </div>
+      <form onSubmit={onSubmit}>
+        <input type="file" accept="image/*" onChange={onImageChange} />
+        {file && (
+          <>
+            <img src={file} width="100px" height="100px" alt="commeet-pic" />
+            <button onClick={onFileClear}>❌</button>
+          </>
+        )}
+        <button>프로필 바꾸기</button>
+        <button onClick={onDeleteImage}>프로필 삭제</button>
+      </form>
+    </>
   );
 };
 
